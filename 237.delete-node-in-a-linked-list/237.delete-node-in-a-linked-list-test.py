ebda37c5b986bceae60335ae8631442ce7922227
lichuachua <1065618302@qq.com>
2023-11-05 00:15:03 +0000

    237 done

---
+++ 237.delete-node-in-a-linked-list/237.delete-node-in-a-linked-list-test.py
@@ -0,0 +1,31 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+from typing import Optional
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        node.val = node.next.val
+        node.next = node.next.next
+
+
+e = ListNode(5, None)
+d = ListNode(4, e)
+c = ListNode(3, d)
+b = ListNode(2, c)
+a = ListNode(1, b)
+
+re = Solution()
+result = re.deleteNode(b)
+while a is not None:
+    print(a.val)
+    a = a.next

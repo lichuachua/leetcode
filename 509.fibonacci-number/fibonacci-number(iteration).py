65ad4cde2fa222102b7c08c06e9316a77c34c7c0
lichuachua <1065618302@qq.com>
2023-12-15 22:16:32 +0000

    509 done

---
+++ 509.fibonacci-number/fibonacci-number(iteration).py
@@ -0,0 +1,10 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        if n < 2 :
+            return n
+        a,b = 0,1
+        i = 2
+        while i <= n:
+            a,b = b,a+b
+            i+=1
+        return b

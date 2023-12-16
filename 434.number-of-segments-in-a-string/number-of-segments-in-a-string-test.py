258ea043260b93902e2986ca328c58837baf2940
lichuachua <1065618302@qq.com>
2023-12-16 01:18:33 +0000

    434 done

---
+++ 434.number-of-segments-in-a-string/number-of-segments-in-a-string-test.py
@@ -0,0 +1,11 @@
+class Solution:
+    def countSegments(self, s: str) -> int:
+        res = 0
+        if s == '':
+            return res
+        elif s[0] != ' ':
+            res = 1
+        for i in range(1,len(s)) :
+            if s[i-1] == ' ' and s[i] != ' ':
+                res += 1
+        return res
---
+++ 434.number-of-segments-in-a-string/number-of-segments-in-a-string.py
@@ -0,0 +1,16 @@
+class Solution:
+    def countSegments(self, s: str) -> int:
+        res = 0
+        if s == '':
+            return res
+        elif s[0] != ' ':
+            res = 1
+        for i in range(1,len(s)) :
+            if s[i-1] == ' ' and s[i] != ' ':
+                res += 1
+        return res
+
+
+
+re = Solution()
+print(re.countSegments(" 123"))

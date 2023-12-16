67e4336f16ea296dd340c2cc6823dec20fa9cfa5
lichuachua <1065618302@qq.com>
2023-12-16 13:47:48 +0000

    387 done

---
+++ 387.first-unique-character-in-a-string/first-unique-character-in-a-string-hash.py
@@ -0,0 +1,10 @@
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        # hash遍历两次
+        h = {}
+        for i in s :
+            h[i] = h.get(i, 0) + 1
+        for i,item in enumerate(s) :
+            if h[item] == 1 :
+                return i
+        return -1
---
+++ 387.first-unique-character-in-a-string/first-unique-character-in-a-string-test.py
@@ -0,0 +1,17 @@
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        queue = [s[0]]
+        low,high = 0,1
+        for i in range(1,len(s)) :
+            if high!=low and queue[low]==s[i]:
+                low+=1
+            else :
+                queue.append(s[i])
+                high+=1
+        if high==low:
+            return -1
+        return low
+
+
+re = Solution()
+print(re.firstUniqChar("aadadaad"))

6ed70230a732b331c2ec9879f2edb0fbea354b9e
lichuachua <1065618302@qq.com>
2023-12-26 21:53:25 +0000

    258 done

---
+++ 258.add-digits/add-digits-recursion.py
@@ -0,0 +1,9 @@
+class Solution:
+    def addDigits(self, num: int) -> int:
+        if num < 10 :
+            return num
+        res = 0
+        while num!=0 :
+            res += num%10
+            num//=10
+        return self.addDigits(res)

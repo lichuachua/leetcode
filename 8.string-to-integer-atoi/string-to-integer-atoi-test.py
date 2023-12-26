b971083b2b5e6d67709e253b0f30dac509ecd7c6
lichuachua <1065618302@qq.com>
2023-12-26 21:30:41 +0000

    8 done

---
+++ 8.string-to-integer-atoi/string-to-integer-atoi-test.py
@@ -0,0 +1,25 @@
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        s = s.strip()
+        if s == "":
+            return 0
+        result = 0
+        flag = 1
+        if s[0] == '-':
+            flag = -1
+        if s[0] == '-' or s[0] == '+':
+            s = s[1:]
+        for i, v in enumerate(s):
+            if v >= '0' and v <= '9':
+                result = result * 10 + int(v)
+            else:
+                break
+            if result >= 2 ** 31:
+                if flag == -1:
+                    return -2 ** 31
+                return 2 ** 31 - 1
+        return flag * result
+
+
+re = Solution()
+print(re.myAtoi("42"))

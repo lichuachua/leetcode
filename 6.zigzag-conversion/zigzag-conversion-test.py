1111a91aa8a2ae57ecb2777bd59bf5c50e1c65bf
lichuachua <1065618302@qq.com>
2023-12-29 13:36:34 +0000

    6 done

---
+++ 6.zigzag-conversion/zigzag-conversion-test.py
@@ -0,0 +1,16 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1:
+            return s
+        res = [''] * len(s)
+        temp = numRows * 2 - 2
+        for i in range(len(s)):
+            index = i % temp
+            if index < numRows:
+                res[index] += s[i]
+            else:
+                res[temp - index] += s[i]
+        return ''.join(res)
+
+re = Solution()
+print(re.convert("PAYPALISHIRING",3))

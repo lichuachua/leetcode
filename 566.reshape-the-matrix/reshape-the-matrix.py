f8046e5c67652220ca84bdef0193f674d9bb8369
lichuachua <1065618302@qq.com>
2023-12-29 21:45:54 +0000

    566 done

---
+++ 566.reshape-the-matrix/reshape-the-matrix.py
@@ -0,0 +1,10 @@
+class Solution:
+    def matrixReshape(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]:
+        if r * c != len(mat) * len(mat[0]):
+            return mat
+        res = [0] * r
+        for i in range(r):
+            res[i] = [0] * c
+            for j in range(c):
+                res[i][j] = mat[(i * c + j) // len(mat[0])][(i * c + j) % len(mat[0])]
+        return res

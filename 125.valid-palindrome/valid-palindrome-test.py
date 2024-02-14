8c84e7de2f8f1dcf792db621c777613a3a6d9ea3
lichuachua <1065618302@qq.com>
2024-02-14 11:20:16 +0000

    125 done

---
+++ 125.valid-palindrome/valid-palindrome-test.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        left, right = 0, len(s) - 1
+        s = s.lower()
+        while left <= right:
+            if not (s[left].isdigit() or s[left].isalpha()):
+                left += 1
+                continue
+            if not (s[right].isdigit() or s[right].isalpha()):
+                right -= 1
+                continue
+            if s[left] != s[right]:
+                return False
+            else:
+                left += 1
+                right -= 1
+        return True
+
+
+re = Solution()
+print(re.isPalindrome("A man, a plan, a canal: Panama"))

d68a459b0b800cb457d888e4c44aa90b7b226ee2
lichuachua <1065618302@qq.com>
2024-02-15 22:31:14 +0000

    9 done

---
+++ 9.palindrome-number/palindrome-number(int reverse).py
@@ -0,0 +1,13 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0 :
+            return False
+        y = x
+        res = 0
+        while x != 0:
+            res = res*10+x%10
+            x = x//10
+        if y == res:
+            return True
+        else:
+            return False
---
+++ 9.palindrome-number/palindrome-number-test.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0 :
+            return False
+        y = x
+        res = 0
+        while x != 0:
+            res = res*10+x%10
+            x = x//10
+        if y == res:
+            return True
+        else:
+            return False
+
+
+re = Solution()
+print(re.isPalindrome(-121))

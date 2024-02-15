8ce0f33e6384f9d58fc7edba0dbdbd68b47eb4fd
lichuachua <1065618302@qq.com>
2024-02-15 22:54:11 +0000

    344 done

---
+++ 344.reverse-string/reverse-string-test.py
@@ -0,0 +1,12 @@
+from typing import List
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        # s[:] = s[::-1] 是对原始字符串就地修改
+        # 而 s = s[::-1] 则是创建一个新的反转字符串并赋值给变量 s
+        s[:] = s[::-1]
+
+re = Solution()
+re.reverseString(["h","e","l","l","o"])

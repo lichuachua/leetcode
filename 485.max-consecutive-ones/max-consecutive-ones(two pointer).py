cffacbf24328d069db2c464e83c0fbacac342e2a
lichuachua <1065618302@qq.com>
2024-02-24 23:48:24 +0000

    485

---
+++ 485.max-consecutive-ones/max-consecutive-ones(two pointer).py
@@ -0,0 +1,15 @@
+class Solution:
+    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        res = 0
+        slow, fast = 0, 0
+        while fast < len(nums):
+            if nums[fast] == 1:
+                fast += 1
+            else:
+                if res < fast - slow:
+                    res = fast - slow
+                fast += 1
+                slow = fast
+        if res < fast - slow:
+            res = fast - slow
+        return res
---
+++ 485.max-consecutive-ones/max-consecutive-ones-test.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+class Solution:
+    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        res = 0
+        slow, fast = 0, 0
+        while fast < len(nums):
+            if nums[fast] == 1:
+                fast += 1
+            else:
+                if res < fast - slow:
+                    res = fast - slow
+                fast += 1
+                slow = fast
+        if res < fast - slow:
+            res = fast - slow
+        return res
+
+re = Solution()
+print(re.findMaxConsecutiveOnes([1,1,0,1,1,1]))

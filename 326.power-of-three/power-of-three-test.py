9cc6d3d1054e88d3bd2b7f8f6686ea1522bfd104
lichuachua <1065618302@qq.com>
2024-05-02 11:31:12 +0100

    2 3 4的幂 done

---
+++ 326.power-of-three/power-of-three-test.py
@@ -0,0 +1,13 @@
+import math
+
+
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        '''找到范围内最大的公约数，除n看余数是否为0
+        【 32 位有符号整数的范围内，3的最大幂为3^19，判断n是否能被 3^19整除】
+        '''
+        return n > 0 and int(math.pow(3, 19)) % n == 0
+
+
+re = Solution()
+print(re.isPowerOfThree(9))

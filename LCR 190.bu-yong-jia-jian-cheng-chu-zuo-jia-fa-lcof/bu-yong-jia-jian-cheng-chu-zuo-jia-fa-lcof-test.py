c057f79b4644129450b58a74f0dec801bc1fec9b
lichuachua <1065618302@qq.com>
2024-05-10 21:55:00 +0100

    LCR 190 done

---
+++ LCR 190.bu-yong-jia-jian-cheng-chu-zuo-jia-fa-lcof/bu-yong-jia-jian-cheng-chu-zuo-jia-fa-lcof-test.py
@@ -0,0 +1,11 @@
+class Solution:
+    def encryptionCalculate(self, dataA: int, dataB: int) -> int:
+        max = 0xffffffff
+        dataA, dataB = dataA & max, dataB & max
+        while dataB != 0:
+            dataA, dataB = (dataA ^ dataB), (dataA & dataB) << 1 & max
+        return dataA if dataA <= 0x7fffffff else ~(dataA ^ max)
+
+
+re = Solution()
+print(re.encryptionCalculate(5, -1))

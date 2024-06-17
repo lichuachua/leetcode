1966f735b2b7a579447c41e771e13c58fd90a119
lichuachua <1065618302@qq.com>
2024-06-17 09:56:06 +0800

    349 done

--- 349.intersection-of-two-arrays/intersection-of-two-arrays(map).py
+++ 349.intersection-of-two-arrays/intersection-of-two-arrays(map).py
@@ -7,5 +7,13 @@ class Solution:
         for i in range(len(nums2) - 1):
             if nums2[i] in m and m[nums2[i]] == 1:
                 res.append(nums2[i])
+                # 避免重复
                 m[nums2[i]] = 0
         return res
+
+
+"""
+Solution:哈希表
+使用一个hash map存储nums1中的元素（出现就标记为1）
+遍历nums2，如果hash map中存在nums2中的元素，则将其添加到结果res中。并且置hash map中的nums2元素为0，防止重复
+"""
--- 349.intersection-of-two-arrays/intersection-of-two-arrays(two pointer).py
+++ 349.intersection-of-two-arrays/intersection-of-two-arrays(two pointer).py
@@ -4,13 +4,14 @@ class Solution:
         nums1.sort()
         nums2.sort()
         i, j = 0, 0
-        while i < len(nums1) - 1 and j < len(nums2) - 1:
-            if nums1[i] == nums1[i + 1]:
+        while i < len(nums1) and j < len(nums2):
+            # 如果当前元素与前一个元素相同，则跳过
+            while i < len(nums1) - 1 and nums1[i] == nums1[i + 1]:
                 i += 1
-                continue
-            if nums2[j] == nums2[j + 1]:
+
+            while j < len(nums2) - 1 and nums2[j] == nums2[j + 1]:
                 j += 1
-                continue
+            # 比较当前元素
             if nums1[i] == nums2[j]:
                 res.append(nums1[i])
                 i += 1
@@ -19,22 +20,19 @@ class Solution:
                 i += 1
             else:
                 j += 1
-        if i == len(nums1) - 1:
-            while j < len(nums2):
-                if nums1[i] == nums2[j]:
-                    res.append(nums1[i])
-                    i += 1
-                    j += 1
-                    break
-                else:
-                    j += 1
-        if j == len(nums2) - 1:
-            while i < len(nums1):
-                if nums2[j] == nums1[i]:
-                    res.append(nums2[j])
-                    i += 1
-                    j += 1
-                    break
-                else:
-                    i += 1
         return res
+
+
+"""
+Solution:双指针
+1.先对2个list进行排序
+2.i和j都从两个数组的0开始，遍历数组
+    先排除两个数组中各自重复的元素
+    再比较两个数组中元素的值
+        相等则放到res
+        nums1的元素小，i++
+        nums2的元素小，j++
+
+
+
+"""

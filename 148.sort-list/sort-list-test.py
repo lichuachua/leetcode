28c395edb6f377c2994a580ffd67cad19760d504
lichuachua <1065618302@qq.com>
2024-07-20 18:44:53 +0100

    148 done

---
+++ 148.sort-list/sort-list-test.py
@@ -0,0 +1,64 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        res = ListNode()
+        tmp = res
+        while list1 and list2:
+            if list1.val < list2.val:
+                tmp.next = list1
+                list1 = list1.next
+            else:
+                tmp.next = list2
+                list2 = list2.next
+            tmp = tmp.next
+        if list1:
+            tmp.next = list1
+        else:
+            tmp.next = list2
+        return res.next
+
+    def mergeSort(self, head: ListNode):
+        # 分割环节
+        if not head or not head.next:
+            return head
+
+        # 快慢指针找到中心链节点
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+            # 断开左右链节点
+        left_head, right_head = head, slow.next
+        slow.next = None
+
+        # 归并操作
+        return self.mergeTwoLists(self.mergeSort(left_head), self.mergeSort(right_head))
+
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        return self.mergeSort(head)
+
+
+d = ListNode(3, None)
+c = ListNode(1, d)
+b = ListNode(2, c)
+a = ListNode(4, b)
+
+re = Solution()
+result = re.sortList(a)
+while result:
+    print(result.val)
+    result = result.next

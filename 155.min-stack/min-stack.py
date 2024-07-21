c7c4da9bb66939bf6f84b98fdd236340d143d606
lichuachua <1065618302@qq.com>
2024-07-21 15:52:58 +0100

    155 done

--- 155.min-stack/min-stack.py
+++ 155.min-stack/min-stack.py
@@ -19,9 +19,19 @@ class MinStack:
                 res = self.stack[i]
         return res
 
+
 # Your MinStack object will be instantiated and called as such:
 # obj = MinStack()
 # obj.push(val)
 # obj.pop()
 # param_3 = obj.top()
 # param_4 = obj.getMin()
+
+"""
+Solution:模拟
+符合栈先进先出的特征进行push、pop、top。
+getMin：循环遍历栈，找到最小值
+其他方法：
+1.栈的元素使用元组，保存当前值和栈中的最小值
+2.辅助栈，保存每次存入新元素时候的栈中的最小值
+"""

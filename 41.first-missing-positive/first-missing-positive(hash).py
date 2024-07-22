a73bc62e291591d7a6ed3bcf8e50e767b3b892b0
lichuachua <1065618302@qq.com>
2024-07-23 00:33:02 +0100

    41 done

---
+++ 41.first-missing-positive/first-missing-positive(hash).py
@@ -0,0 +1,20 @@
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        size = len(nums)
+
+        # 第一步：将每个正整数放到它应该在的位置上
+        for i in range(size):
+            # 当 nums[i] 在 [1, size] 范围内，并且 nums[i] 不在正确的位置上时
+            while 1 <= nums[i] <= size and nums[i] != nums[nums[i] - 1]:
+                # 交换 nums[i] 和 nums[nums[i] - 1]
+                index1 = i
+                index2 = nums[i] - 1
+                nums[index1], nums[index2] = nums[index2], nums[index1]
+
+        # 第二步：遍历数组，找到第一个不在正确位置上的元素
+        for i in range(size):
+            if nums[i] != i + 1:
+                return i + 1
+
+        # 如果所有位置上的元素都在正确的位置上，则缺失的正整数是 size + 1
+        return size + 1
---
+++ 41.first-missing-positive/first-missing-positive(traversal).py
@@ -0,0 +1,31 @@
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        n = len(nums)
+
+        # 第一步：将不在范围 [1, n] 的元素设置为 n + 1
+        for i in range(n):
+            if nums[i] <= 0 or nums[i] > n:
+                nums[i] = n + 1
+
+        # 第二步：通过标记元素的存在性
+        for i in range(n):
+            num = abs(nums[i])
+            if 1 <= num <= n:
+                nums[num - 1] = -abs(nums[num - 1])
+
+        # 第三步：查找第一个缺失的正整数
+        for i in range(n):
+            if nums[i] > 0:
+                return i + 1
+
+        # 如果所有位置的值都是负数，则缺失的正整数是 n + 1
+        return n + 1
+
+
+"""
+Solution：遍历
+# 第一步：将不在范围 [1, n] 的元素设置为 n + 1
+# 第二步：通过标记元素的存在性
+# 第三步：查找第一个缺失的正整数---------通过将对应索引的元素变为负数来标记某个正整数是否存在。
+# 如果所有位置的值都是负数，则缺失的正整数是 n + 1
+"""

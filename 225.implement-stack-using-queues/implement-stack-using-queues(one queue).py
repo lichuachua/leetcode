10841e21919517ac72bc42c67401ce03bc302ab7
lichuachua <1065618302@qq.com>
2024-07-22 23:42:39 +0100

    225 done

--- 225.implement-stack-using-queues/implement-stack-using-queues(one queue).py
+++ 225.implement-stack-using-queues/implement-stack-using-queues(one queue).py
@@ -21,9 +21,21 @@ class MyStack:
     def empty(self) -> bool:
         return len(self.queue) == 0
 
+
 # Your MyStack object will be instantiated and called as such:
 # obj = MyStack()
 # obj.push(x)
 # param_2 = obj.pop()
 # param_3 = obj.top()
 # param_4 = obj.empty()
+
+"""
+Solution：单个队列
+使用单个队列实现栈
+push 方法接收一个整数 x 并将其压入栈中。
+    首先获取当前队列的长度 n。
+    将元素 x 追加到队列末尾。
+    进行 n 次操作
+    将队列第一个元素添加到队列末尾
+    移除队列第一个元素
+"""
--- 225.implement-stack-using-queues/implement-stack-using-queues(two queue).py
+++ 225.implement-stack-using-queues/implement-stack-using-queues(two queue).py
@@ -22,9 +22,20 @@ class MyStack:
     def empty(self) -> bool:
         return len(self.queue1) == 0
 
+
 # Your MyStack object will be instantiated and called as such:
 # obj = MyStack()
 # obj.push(x)
 # param_2 = obj.pop()
 # param_3 = obj.top()
 # param_4 = obj.empty()
+
+"""
+Solution：两个队列
+使用辅助队列实现栈
+pop 方法弹出并返回栈顶元素。
+    首先，将 queue1 中除了最后一个元素之外的所有元素移动到 queue2。
+    然后，保存 queue1 的最后一个元素（栈顶元素）到 res。
+    接下来，交换 queue1 和 queue2，并清空 queue2。
+    最后，返回栈顶元素 res。
+"""

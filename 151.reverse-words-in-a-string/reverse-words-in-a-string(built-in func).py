24866c5ac1966d68fbf100e1665c3f0fac4df543
lichuachua <1065618302@qq.com>
2024-07-27 22:43:44 +0100

    151done

--- 151.reverse-words-in-a-string/reverse-words-in-a-string(built-in func).py
+++ 151.reverse-words-in-a-string/reverse-words-in-a-string(built-in func).py
@@ -2,3 +2,10 @@ class Solution:
     def reverseWords(self, s: str) -> str:
         s = s.split()
         return ' '.join(s[::-1])
+
+
+"""
+Solution：内置函数
+同557
+使用内置方法将字符串切分为单词，之后使用内置方法将每个单词逆转，再将单词合并为字符串
+"""
--- 151.reverse-words-in-a-string/reverse-words-in-a-string(two pointer).py
+++ 151.reverse-words-in-a-string/reverse-words-in-a-string(two pointer).py
@@ -21,3 +21,11 @@ class Solution:
         if word:
             words.append(word)
         return words
+
+
+"""
+Solution：对撞指针
+同344题
+使用空格把字符串切分为单词，之后对每一个单词进行反转，
+反转使用对撞指针，从0, len(s) - 1开始往中间靠拢，相互交换
+"""

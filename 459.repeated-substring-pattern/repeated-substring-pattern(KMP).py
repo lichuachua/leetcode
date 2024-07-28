fe27440ee43a25469c774291d64253a9c5626201
lichuachua <1065618302@qq.com>
2024-07-28 11:24:48 +0100

    459 done

--- 459.repeated-substring-pattern/repeated-substring-pattern(KMP).py
+++ 459.repeated-substring-pattern/repeated-substring-pattern(KMP).py
@@ -1,15 +1,28 @@
 class Solution:
+    def generateNext(self, p: str):
+        m = len(p)
+        next = [0 for _ in range(m)]
+
+        left = 0
+        for right in range(1, m):
+            while left > 0 and p[left] != p[right]:
+                left = next[left - 1]
+            if p[left] == p[right]:
+                left += 1
+            next[right] = left
+
+        return next
+
     def repeatedSubstringPattern(self, s: str) -> bool:
-        n = len(s)
-        for i in range(1, n // 2 + 1):
-            if n % i == 0:
-                substring = s[:i]
-                if substring * (n // i) == s:
-                    return True
+        size = len(s)
+        if size == 0:
+            return False
+        next = self.generateNext(s)
+        if next[size - 1] != 0 and size % (size - next[size - 1]) == 0:
+            return True
         return False
 
-
 """
-Solution：枚举
-遍历字符串，假设字符串是由子字符串组成的，那么n % i == 0，之后比较子字符串*多次能等于字符串
+Solution：KMP
+TODO
 """

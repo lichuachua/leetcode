6503f812a68c3c80e8baa571a0747721c81852fa
lichuachua <1065618302@qq.com>
2024-07-28 22:16:38 +0100

    145 done

--- 145.binary-tree-postorder-traversal/binary-tree-postorder-traversal(iteration)/binary-tree-postorder-traversal(iteration).py
+++ 145.binary-tree-postorder-traversal/binary-tree-postorder-traversal(iteration)/binary-tree-postorder-traversal(iteration).py
@@ -18,6 +18,8 @@ class Solution:
                 root = root.left
             root = stack[len(stack) - 1]
             stack.pop()
+            # 如果右子树不存在 或 右子树已经被访问过：可以将节点值加入结果列表。
+            # 否则：将当前节点重新入栈，并将 root 更新为右子树进行处理。
             if root.right is None or root.right == preRight:
                 res.append(root.val)
                 preRight = root
@@ -26,3 +28,13 @@ class Solution:
                 stack.append(root)
                 root = root.right
         return res
+
+
+"""
+Solution：迭代+栈
+通过迭代调用左->右->根的顺序实现二叉树的后序遍历
+同中序遍历一样，向左遍历：将所有左子树节点推入栈中。
+处理节点：
+如果右子树不存在 或 右子树已经被访问过：可以将节点值加入结果列表。
+否则：将当前节点重新入栈，并将 root 更新为右子树进行处理。
+"""
--- 145.binary-tree-postorder-traversal/binary-tree-postorder-traversal-test.py
+++ 145.binary-tree-postorder-traversal/binary-tree-postorder-traversal-test.py
@@ -4,6 +4,15 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+from typing import List, Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
 class Solution:
     def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         if root is not None:
@@ -12,7 +21,13 @@ class Solution:
             return []
 
 
-"""
-Solution：递归
-通过递归调用左->右->根的顺序实现二叉树的前序遍历
-"""
+a, b, c = TreeNode(1), TreeNode(2), TreeNode(3)
+a.left = None
+a.right = b
+b.left = c
+b.right = None
+c.right = None
+c.left = None
+
+re = Solution()
+print(re.postorderTraversal(a))

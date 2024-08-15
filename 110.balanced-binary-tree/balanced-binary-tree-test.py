d6202bc88cc9eec8684788966020fb7beda1b732
lichuachua <1065618302@qq.com>
2024-08-15 23:24:47 +0100

    110

--- 110.balanced-binary-tree/balanced-binary-tree-test.py
+++ 110.balanced-binary-tree/balanced-binary-tree-test.py
@@ -1,7 +1,7 @@
+# Definition for a binary tree node.
 from typing import Optional
 
 
-# Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
@@ -9,45 +9,33 @@ class TreeNode:
         self.right = right
 
 
-def build_binary_tree():
-    # 创建节点
-    root = TreeNode(1)
-
-    return root
-
-
-def print_binary_tree(root, level=0, prefix="Root: "):
-    if root is not None:
-        print(" " * (level * 4) + prefix + str(root.val))
-        if root.left is not None or root.right is not None:
-            print_binary_tree(root.left, level + 1, "L--- ")
-            print_binary_tree(root.right, level + 1, "R--- ")
-
-
-# 构建二叉树
-binary_tree_root = build_binary_tree()
-
-# 打印二叉树
-print_binary_tree(binary_tree_root)
-
-
 class Solution:
+    def height(self, root: Optional[TreeNode]) -> int:
+        if root == None:
+            return 0
+        leftHeight = self.height(root.left)
+        rightHeight = self.height(root.right)
+        if leftHeight == -1 or rightHeight == -1 or abs(leftHeight - rightHeight) > 1:
+            return -1
+        else:
+            return max(leftHeight, rightHeight) + 1
 
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        if root is None:
-            return True
+        return self.height(root) >= 0
 
-        def maxDepth(root):
-            if root is None:
-                return True
-            return max(maxDepth(root.left), maxDepth(root.right)) + 1
 
-        level = abs(maxDepth(root.right) - maxDepth(root.left))
-        if level <= 1:
-            return True
-        else:
-            return False
-
+a, b, c, d, e = TreeNode(3), TreeNode(9), TreeNode(20), TreeNode(15), TreeNode(7)
+a.left = b
+a.right = c
+b.left = None
+b.right = None
+c.right = d
+c.left = e
+d.right = None
+d.left = None
+e.right = None
+e.left = None
 
 re = Solution()
-print(re.isBalanced(binary_tree_root))
+res = re.isBalanced(a)
+print(res)
--- 110.balanced-binary-tree/balanced-binary-tree.py
+++ 110.balanced-binary-tree/balanced-binary-tree.py
@@ -5,15 +5,29 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    def height(self, root: Optional[TreeNode]) -> int:
+        if root == None:
+            return 0
+        leftHeight = self.height(root.left)
+        rightHeight = self.height(root.right)
+        if leftHeight == -1 or rightHeight == -1 or abs(leftHeight - rightHeight) > 1:
+            return -1
+        else:
+            return max(leftHeight, rightHeight) + 1
 
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        if root is None:
-            return True
+        return self.height(root) >= 0
 
-        def maxDepth(root):
-            if root is None:
-                return True
-            return max(maxDepth(root.left), maxDepth(root.right)) + 1
 
-        level = abs(maxDepth(root.right) - maxDepth(root.left))
-        return level <= 1 and self.isBalanced(root.left) and self.isBalanced(root.right)
+"""
+Solution：递归
+先递归遍历左右子树，判断左右子树是否平衡，再判断以当前节点为根节点的左右子树是否平衡。
+如果遍历的子树是平衡的，则返回它的高度，否则返回 -1。
+只要出现不平衡的子树，则该二叉树一定不是平衡二叉树。
+
+height 函数：
+这是一个递归函数，用于计算当前节点的高度。
+如果当前节点为空，返回高度为 0。
+递归计算左右子树的高度。如果某个子树不平衡（返回 -1），或者当前节点的左右子树高度差超过 1，直接返回 -1 表示该树不平衡。
+如果左右子树都平衡，则返回当前节点的高度（即左右子树高度的最大值加上 1）。
+"""
